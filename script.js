6ec40ef8efc532181019802d212733b0d4180ecc
boxerdinesh88-png <boxerdinesh88@gmail.com>
2026-01-11 22:31:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const num1=[100,31,21,12,];
+ const x = Math.min.apply(null,num1);
+console.log(x);
+
+
+
+const num2=[100,31,21,12,];
+ const a = Math.max.apply(null,num2);
+console.log(a);
+
+const months = [ "Feb", "Mar","Jan", "Apr"];
+ months.toSorted();
+console.log(months)
+
+
+
+const hello = ["Jan", "Feb", "Mar", "Apr"];
+hello.sort()
+console.log(hello)
+
+
+const nuum1=5;
+const nuum2 ="7";
+const sum= nuum1+nuum2;
+console.log(sum)
+
+
+const age=17;
+const votercard = age >=18&& 90;
+console.log(votercard)
